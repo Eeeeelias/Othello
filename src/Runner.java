0d70a3e3eff32e0421be7ae383bfd53c31dd6b4e
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2022-08-22 21:15:14 +0200

    Final update

--- src/Runner.java
+++ src/Runner.java
@@ -1,93 +1,91 @@
-import szte.mi.GameBoard;
-import szte.mi.Move;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.Objects;
 
-public class Runner {
+public class Runner extends Application implements EventHandler<ActionEvent> {
 
-    public static void  printBoard(Integer[][] board){
-        System.out.println(" |1|2|3|4|5|6|7|8|");
-        for (int i = 0; i < board.length; i++){
-            System.out.print((i+1) + "|");
-            for (int j = 0; j < board.length; j++){
-                if (board[i][j] == null){
-                    System.out.print(" ");
-                } else{
-                    System.out.print(board[i][j]);
-                }
-                if (j < board.length){
-                    System.out.print("|");
-                }
-            }
-            System.out.print("\n");
+    GUI gui = new GUI();
+    int mode = 5;
+    int order = 5;
+    Button multi = new Button("Play against \n another player");
+    Button single = new Button("Play against \n an AI");
+    Button orderBlack = new Button("Player should \n start");
+    Button orderWhite = new Button("AI should \n start");
+
+    @Override
+    public void start(Stage stage) {
+        //Set up the first window so the player can decide whether to play with another player or the AI
+        GridPane selectMode;
+        selectMode = makeInitButtons();
+        selectMode.setStyle("-fx-background-color: green;");
+        selectMode.setPadding(new Insets(50, 50, 50, 50));
+        stage.setTitle("Select Gamemode");
+        stage.setScene(new Scene(selectMode, Color.GREEN));
+        stage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        Button b = (Button) event.getSource();
+        Stage stage1 = new Stage();
+        if (Objects.equals(b.getText(), "Play against \n an AI")){
+            mode = 1;
+            b.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white; -fx-border-color: #413bed; -fx-border-width: 4");
+        } else if(Objects.equals(b.getText(), "Play against \n another player")){
+            mode = 2;
+        }
+        if (Objects.equals(b.getText(), "AI should \n start")){
+            order = 0;
+            b.setStyle("-fx-background-color: white; -fx-border-color: #413bed; -fx-border-width: 4");
+        } else if(Objects.equals(b.getText(), "Player should \n start")){
+            order = 1;
+            b.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white; -fx-border-color: #413bed; -fx-border-width: 4");
+        }
+        if(order != 5 && mode != 5 || mode == 2){
+            BorderPane mainLayout = gui.initGUI(0, mode, order);
+            order = 5;
+            mode = 5;
+            multi.setStyle("-fx-background-color: white");
+            single.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white;");
+            orderBlack.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white;");
+            orderWhite.setStyle("-fx-background-color: white");
+            stage1.setTitle("Othello");
+            stage1.setScene(new Scene(mainLayout, Color.GREEN));
+            stage1.show();
         }
     }
 
-    public static void printMoves (ArrayList<Move> moves){
-        System.out.println("Valid moves: ");
-        if(moves.size() > 0){
-            for (Move m: moves){
-                    System.out.println("row: " + (m.x+1) + " column: " +  (m.y+1));
-            }
-        } else{
-            System.out.println("No valid moves");
-        }
+    public GridPane makeInitButtons(){
+        multi.setPrefSize(100, 100);
+        multi.setStyle("-fx-background-color: white");
+        multi.addEventHandler(ActionEvent.ACTION, this);
+        single.addEventHandler(ActionEvent.ACTION, this);
+        single.setPrefSize(100, 100);
+        single.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white;");
+        orderBlack.setPrefSize(100,100);
+        orderBlack.setStyle("-fx-background-color: #1e2021; -fx-text-fill: white;");
+        orderBlack.addEventHandler(ActionEvent.ACTION, this);
+        orderWhite.setPrefSize(100,100);
+        orderWhite.setStyle("-fx-background-color: white");
+        orderWhite.addEventHandler(ActionEvent.ACTION, this);
+        GridPane pane = new GridPane();
+        pane.add(multi, 0, 0);
+        pane.add(single, 1, 0);
+        pane.add(orderBlack, 0, 1);
+        pane.add(orderWhite, 1, 1);
+        return pane;
     }
 
     public static void main(String[] args) {
-        GameBoard newGame = new GameBoard(0);
-        Integer[][] testBoard = newGame.getBoard();
-        printBoard(testBoard);
-        int counter = 0;
-        Move prefMove = null;
-
-        while (true){
-            try {
-                Scanner scan = new Scanner(System.in);
-                System.out.println(" Player " + counter%2 +" please type in row & col number");
-                ArrayList<Move> possibleMoves = new ArrayList<>();
-                for(int i = 0; i < 8; i++){
-                    for (int j = 0; j <8; j++){
-                        //ERROR: adds the changes to the possibleMoves not the valid moves
-                        if (testBoard[i][j] == null){
-                            if(newGame.getPointsToChange(new Move(i, j), counter) != null)
-                            possibleMoves.add(new Move(i, j));
-                        }
-                    }
-                }
-                possibleMoves.removeAll(Collections.singleton(null));
-                printMoves(possibleMoves);
-                Move currMove;
-                if(possibleMoves.size() <= 0){
-                    currMove = null;
-                } else {
-                    int x_coord = scan.nextInt();
-                    int y_coord = scan.nextInt();
-                    currMove = new Move(x_coord - 1, y_coord - 1);
-                    newGame.makeMove(currMove, counter);
-                }
-                if(newGame.checkForWinner(currMove, prefMove)){
-                    System.out.println("The Winner is: Player "+ counter %2);
-                    System.exit(0);
-                }
-                printBoard(testBoard);
-                counter++;
-                prefMove = currMove;
-            } catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("invalid move, out of field");
-            } catch (NullPointerException e){
-                e.printStackTrace();
-                System.out.println("invaid move, empty field");
-                counter--;
-            } catch (InputMismatchException e){
-                System.out.println("invalid move, full field");
-                //counter--;
-            } catch (Exception e) {
-                System.out.println("invalid move");
-            }
-        }
+        launch(args);
     }
 }
